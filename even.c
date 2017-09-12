c215b5beaaec0fb150edefd629a19ba35c1c2f1d
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2017-09-12 15:19:13 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+   printf("Enter the value");
+  scanf("%d",&a);
+  if((a%2)==0)
+  {
+      printf("The given number is even");
+  }
+else
+{
+    printf("The Given number is odd");
+}
+getch();
+}
