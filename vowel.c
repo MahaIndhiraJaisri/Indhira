c2171180e5d454098e6c29f1ab22e7501dfe36b6
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2017-09-12 16:04:52 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char c;
+printf("Enter the character");
+scanf("%c",&c);
+if((c=='a')||(c=='A')||(c=='e')||(c=='E')||(c=='i')||(c=='I')||(c=='o')||(c=='O')||(c=='u')||(c=='U'))
+{
+    printf("The given number is vowels");
+}
+else
+{
+    printf("The given number is constant");
+}
+}
