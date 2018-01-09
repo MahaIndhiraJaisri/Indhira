f15ea99530f7a68d140e1d0c90c972cc12f0267c
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-09 16:03:34 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("Enter the N values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n HELLO WORLD");
+}
+getch();
+}
