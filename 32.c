d456774bc86b67c72d26a91b366262a1a4af3591
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-21 11:17:28 +0530

    Create 32.c

---
+++ 32.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a[i],max;
+printf("Enter the N value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+max=a[0];
+for(i=1;i<n;i++)
+{
+if(a[i]>max)
+max=a[i];
+}
+printf("The maximum value is %d",max);
+}
+
+
+
