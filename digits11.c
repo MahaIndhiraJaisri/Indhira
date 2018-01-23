092bb172057717a6666b774e8af474323b346c40
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-23 16:21:42 +0530

    Create digits11.c

---
+++ digits11.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[30];
+printf("ENTER THE VALUE");
+scanf("%c"&c);
+if(isdigit(c)==0)
+{
+printf("%c NOT NUMERIC");
+}
+else
+{
+printf("%c NUMERIC");
+}
+}
+
