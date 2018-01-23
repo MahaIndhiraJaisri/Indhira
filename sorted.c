78edd122b21adee739586a5ead4f69480cfde4f3
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-23 15:48:41 +0530

    Create sorted.c

---
+++ sorted.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n,j;
+scanf("%d",&n);
+printf("THE INPUT OF THE ARRAY VALUE");
+for(i=0i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("THE OUTPUT OF AN ARRAY VALUE");
+for(j=0;j<n;j++)
+{
+printf("%d",b[j]);
+}
+return 0;
+}
