6d6a2b3fb62b7f328e054cef27b3da9390609980
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-25 16:33:51 +0530

    Create space.c

---
+++ space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int i,s;
+length=strlen(a);
+printf("Enter the string");
+scanf("%s",&a);
+for(i=o;i<=length;i++)
+{
+if(a[i]=='')
+{
+s++;
+}
+printf("The space of the string %d",s);
+}
