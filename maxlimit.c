db5689e5a5840a400e5b32123d51c7907201c786
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-01-30 11:26:17 +0000

    Create maxlimit.c

---
+++ maxlimit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int i,a[10],max;
+printf("ENTER THE NUMBER":);
+for(i=0;i<=9;i++)
+{
+scanf("%d",&a[i]);
+}
+if(a[i]>=max)
+{
+a[i]=max;
+}
+else
+{
+count++;
+}
+}
+
