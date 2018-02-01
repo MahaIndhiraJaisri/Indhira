c1e77545ada4d8506f3bc45d72b84976933fcc25
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-02-01 07:48:27 +0000

    Create fibnacci.c

---
+++ fibnacci.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n1,t1,t2,i,n;
+printf("ENTER THE NUMBER"0;
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+printf("ENTER t1);
+n1=t1+t2;
+t1=t2;
+t2=n1;
+}
+return 0;
+}
+
+
