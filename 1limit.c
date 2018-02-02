3bc4e682801692c06e88b7fa01be28323f25bfd6
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-02-02 14:30:59 +0530

    Create 1limit.c

---
+++ 1limit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("ENTER THE NUMBER:");
+scanf("%d",&n);
+if(n<10000000)
+{
+printf("THE NEXT VALUE OF N IS:%d",(n+1));
+}
+else
+{
+printf("ENTER INTO THE LIMIT");
+}
+
+}
