7203e091114e437efc1dffa96b415261b9b9726c
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-02-13 16:17:45 +0530

    Create mul7.c

---
+++ mul7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n;
+printf("ENTER THE VALUE\n");
+scanf("%d",&n);
+if((n%7)==0)
+{
+    printf("YES");
+}
+else
+{
+    printf("NO");
+}
+
+
+    return 0;
+}
