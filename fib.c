5a79f3713005caced19d070cef6e7d5533cdcb03
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-02-22 16:57:30 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 void main()
 {
-int f=0,s=1,N,i,temp;
+int f1=0,s=1,N,i,temp;
 scanf("%d",&N);
 printf("FIBONACCI SERIES....");
-printf("%d%d",f,s);
+printf("%d%d",f1,s);
 for(i=2;i<=N;i++)
 {
-temp=f+s;
-f=f+s;
+temp=f1+s;
+f=f1+s;
 s=s+temp;
 printf("%d",temp);
 }
