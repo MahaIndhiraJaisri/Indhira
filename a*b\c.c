9ce117e51fabd73265b42263bf4c169b68f6abc1
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-03-01 16:55:53 +0530

    Create a*b\c.c

---
+++ a*b\c.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b,c;
+    printf("ENTER THE VALUE a:\n");
+    scanf("%d",&a);
+    printf("ENTER THE VALUE b:\n");
+    scanf("%d",&b);
+    printf("ENTER THE VALUE c:\n");
+    scanf("%d",&c);
+    printf("THE VALUE IS:%d",((a*b)/c));
+}
