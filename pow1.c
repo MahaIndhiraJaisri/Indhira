7010ef3b554652eb7477b6df572b7197ebf54728
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-03-01 16:28:01 +0530

    Create pow1.c

---
+++ pow1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+   int a,d,c;
+   printf("ENTER THE NUMBER!\n");
+   scanf("%d",&a);
+   printf("ENTER THE NUMBER2\n");
+   scanf("%d",&d);
+   c=pow(a,d);
+   printf("THE POWER OF THE NUMBER IS:%d",c);
+   return 0;
+}
