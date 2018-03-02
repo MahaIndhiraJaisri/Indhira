3cfef4dd0f6942b013f005437c43d0a8f4e6c2cf
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-03-02 14:37:21 +0530

    Create kelvin.c

---
+++ kelvin.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	float cel1,kel;
+	printf("Enter the Degree of Celsius:");
+	scanf("%f",&cel1);
+	kel=cel1+273.15;
+	printf("The Temperature in Kelvin is %f",kel);
+}
