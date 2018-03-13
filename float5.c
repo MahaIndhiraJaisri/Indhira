1faf2a2198f37588949318c81763ab8d1dca5b49
MahaIndhiraJaisri <30829768+MahaIndhiraJaisri@users.noreply.github.com>
2018-03-13 15:51:34 +0530

    Create float5.c

---
+++ float5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float l,b,area1;
+    printf("ENTER THE LENGTH\n");
+    scanf("%f",&l);
+    printf("ENTER THE BREADTH\n");
+    scanf("%f",&b);
+    area1=l*b;
+    printf("\nTHE AREA OF THE FEILD IS:%.5f",area1);
+    
+}
